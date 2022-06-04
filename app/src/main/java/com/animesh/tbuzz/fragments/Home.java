04dd6ae421f36db099768773592d78b63af1eef0
Animesh Rajak <animeshr4720@gmail.com>
2022-06-04 22:16:11 +0530

    main actitity starting

---
+++ app/src/main/java/com/animesh/tbuzz/fragments/Home.java
@@ -0,0 +1,40 @@
+package com.animesh.tbuzz.fragments;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.animesh.tbuzz.R;
+
+
+public class Home extends Fragment {
+
+    private RecyclerView recyclerView;
+
+    HomeAdapter adapter;
+
+    public Home() {
+        // Required empty public constructor
+    }
+
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_home, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+}
